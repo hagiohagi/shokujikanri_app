4938f0356bd6aa9c2d3beea65e208ada525e5f70
hagiohagi <hagigyyyyyyy@gmail.com>
2022-05-05 12:08:11 +0900

    ルートディレクトリへのアクセスはログイン画面にリダイレクトの実装

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,19 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+
+
+// ルートディレクトリへのアクセスはログイン画面にリダイレクト
+Route::redirect('/', '/login');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
