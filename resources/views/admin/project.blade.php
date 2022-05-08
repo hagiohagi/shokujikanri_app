0a4214a4a17bcf20c3c6257118d10eb79aee3052
hagiohagi <hagigyyyyyyy@gmail.com>
2022-05-08 11:02:29 +0900

    管理者側で編集機能の追加

--- resources/views/admin/project.blade.php
+++ resources/views/admin/project.blade.php
@@ -3,6 +3,10 @@
 @section('content')
 <div class="container">
 @csrf
+<ul class="nav nav-tabs">
+    <li class="nav-item"><a class="nav-link disabled" href="/admin/project">調査一覧</a></li>
+    <li class="nav-item "><a class="nav-link" href="/admin/user">ユーザー一覧</a></li>
+</ul>
   
     <h1>調査一覧</h1>
 
@@ -18,7 +22,7 @@
               <th>年代</th>
               <th>性別</th>
               <th>競技</th>
-
+              <th></th>
           </tr>
       </thead>
       @foreach($survey_info as $data)
@@ -29,7 +33,7 @@
               <td>{{ $data->era }}</td>
               <td>{{ $data->sex }}</td>
               <td>{{ $data->sport }}</td>
-             
+              <td><a class="btn btn-secondary" href="/admin/project/{{ $data->survey_id }}/edit">編集する</a></td>
           </tr>
       </tbody>
       @endforeach
