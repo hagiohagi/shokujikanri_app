921243673cd4597c6a8e60797aaba17dcfa541a2
hagiohagi <hagigyyyyyyy@gmail.com>
2022-05-14 17:18:22 +0900

    indexの食事表示仮作成

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MealRecord;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index(Request $request)
     {
-        return view('index');
+
+        $meals = MealRecord::where('user_id', Auth::user()->id)->get();
+
+        return view('index',['meals' => $meals]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,17 @@
     <a href="#" class="h2 text-dark "><u>調査に関する注意</u></a>
   </div>
 
+  @foreach($meals as $meal)
+  <div class="container mt-3 mx-2 border">
+  <div class="my-box w-25">{{$meal->meal_type}}</div>
+  <div class="col-7 mt-3">col-7</div>
+    <div class="d-flex justify-content-start">
+      <div class="my-box w-25">{{$meal->meal_date}}{{$meal->meal_time}}</div>
+      <div class="my-box w-25">{{$meal->eat_place}}</div>
+    </div>
+  </div>
+  @endforeach
+
     <a href="/upload">
       <input type="button" class="mt-5 btn btn-secondary col-sm-2" value="回答を追加する">
     </a>
