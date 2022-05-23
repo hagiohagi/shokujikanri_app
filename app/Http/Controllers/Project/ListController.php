a5bf03e09b3a7d25fcbc48510fdda480a13caf23
hagiohagi <hagigyyyyyyy@gmail.com>
2022-05-23 21:58:04 +0900

    一覧の投稿の項目ざっと作成

--- app/Http/Controllers/Project/ListController.php
+++ app/Http/Controllers/Project/ListController.php
@@ -10,7 +10,7 @@ class ListController extends Controller
 {
     public function index(Request $request,$survey_id)
     {
-        $survey_info = SurveyInfo::find($survey_id);
+        $survey_info = SurveyInfo::with('users.mealrecords','users.mealrecords.mealPhotos','users.mealrecords.mealDetails')->find($survey_id);
         return view('project.list',['survey_info' => $survey_info]);
     }
 }
--- resources/views/project/list.blade.php
+++ resources/views/project/list.blade.php
@@ -13,10 +13,12 @@
         <label class="control-label">
         絞込み：
         </label>
-          <select class="form-control">
-            <option value="" hidden>名前を選択してください</option>
-            <option value="1">田中太郎</option>
-            <option value="2">山田花子</option>
+
+        <select class="form-control">
+          <option value="" hidden>名前を選択してください</option>
+          @foreach($survey_info->users as $user)
+            <option value="{{$user->id}}">{{$user->name}}</option>
+          @endforeach
         </select>
         <!-- @error('')
             <span class="invalid-feedback" role="alert">
@@ -49,7 +51,34 @@
       <div class="my-box w-25">自宅</div>
     </div>
     <div class="row">
-      <div class="col-7 mt-3">col-7</div>
+      <div class="col-7 mt-3">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="/images/upload.png" class="d-block w-100" alt="/images/upload.png">
+            </div>
+            <div class="carousel-item">
+              <img src="/images/upload.png" class="d-block w-100" alt="/images/upload.png">
+            </div>
+            <div class="carousel-item">
+              <img src="/images/upload.png" class="d-block w-100" alt="/images/upload.png">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="false"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
       <div class="col-5 mt-3">
         <table class="table display-none">
           <tr>
@@ -64,6 +93,62 @@
       </div>
     </div>
   </div>
+
+  @foreach($survey_info->users as $user)
+  @foreach($user->mealrecords as $mealrecord)
+  <div class="container mt-3 mx-2 border">
+    <div class="d-flex justify-content-start">
+      <div class="my-box w-25">{{$user->name}}</div>
+      <div class="my-box w-25">朝食</div>
+      <div class="my-box w-25">{{$mealrecord->eat_date}}{{$mealrecord->eat_time}}</div>
+      <div class="my-box w-25">{{$mealrecord->eat_place}}</div>
+    </div>
+    <div class="row">
+      <div class="col-7 mt-3">
+      @foreach($mealrecord->mealPhotos as $meal_photo)
+
+      @endforeach
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+          @foreach($mealrecord->mealPhotos as $meal_photo)
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="{{$meal_photo->photo_num - 1}}" class="active" aria-current="true" aria-label="Slide {{$meal_photo->photo_num}}"></button>
+            @endforeach
+          </div>
+          <div class="carousel-inner">
+            @foreach($mealrecord->mealPhotos as $meal_photo)
+            <div class="carousel-item">
+              <img src="{{ url('/images/'. $meal_photo->photo_path)}}" class="d-block w-100" alt="/images/upload.png">
+            </div>
+            @endforeach
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="false"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div class="col-5 mt-3">
+        <table class="table display-none">
+        @foreach($mealrecord->mealDetails as $meal_detail)
+          <tr>
+              <td>{{$meal_detail->food}}</td>
+              <td>{{$meal_detail->ingredient}}</td>
+              <td>{{$meal_detail->amount}}</td>
+          </tr>
+          @endforeach
+        </table>
+        <div>
+        {{$mealrecord->memo}}
+        </div>
+      </div>
+    </div>
+  </div>
+  @endforeach
+  @endforeach
 
   <a class="mt-5 btn btn-secondary" href="/project/index">一覧に戻る</a>
 
