658b108db10d435f3b9e464d6f6776ff291198e7
hagiohagi <hagigyyyyyyy@gmail.com>
2022-06-11 11:59:22 +0900

    並び替え仮実装

--- app/Http/Controllers/Project/ListController.php
+++ app/Http/Controllers/Project/ListController.php
@@ -20,22 +20,27 @@ class ListController extends Controller
     {
         $survey_info = SurveyInfo::with('users', 'users.mealrecords', 'users.mealrecords.mealPhotos', 'users.mealrecords.mealDetails')->find($survey_id);
         $users = $survey_info->users;
-        
 
         if (isset($request->user_name)) {
             $users = $users->where('name', '=', $request->user_name);
         }
 
-        // if (isset($request->survey_sort)){
-        //     if($request->survey_sort == 1){
-        //         $survey_info = $survey_info->latest();
-        //     }elseif($request->survey_sort == 2){
-        //         $survey_info = $survey_info->oldest();
-        //     }elseif($request->survey_sort == 3){
-        //         $survey_info = $survey_info->sortBy('users.name');
-        //     }
-        // }
+        if (isset($request->survey_sort)) {
 
+            if ($request->survey_sort == 1) {
+                foreach ($users as $user) {
+                    $meal_records = $user->mealrecords;
+                    $user->mealrecords = $meal_records->sortByDesc('created_at')->values();
+                }
+            } elseif ($request->survey_sort == 2) {
+                foreach ($users as $user) {
+                    $meal_records = $user->mealrecords;
+                    $user->mealrecords = $meal_records->sortBy('created_at')->values();
+                }
+            } elseif ($request->survey_sort == 3) {
+                $users = $users->sortBy('name')->values();
+            }
+        }
 
         return view('project.list', ['survey_info' => $survey_info, 'users' => $users]);
     }
