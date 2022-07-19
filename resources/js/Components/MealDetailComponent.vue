6d3b9824918f1cd405a54d2653f42e3419bd2f07
hagiohagi <hagigyyyyyyy@gmail.com>
2022-07-19 21:03:41 +0900

    食事詳細コンポーネントの記述を編集

--- resources/js/Components/MealDetailComponent.vue
+++ resources/js/Components/MealDetailComponent.vue
@@ -1,30 +1,13 @@
 <template>
   <div>
     <div class="col-12 mb-3 row d-flex justify-content-between">
-      <div class="form-group col-4" v-for="(food, index) in foods" :key="index">
-        <input class="form-control" type="text" v-model="foods[index]" />
-      </div>
-      <div
-        class="form-group col-4"
-        v-for="(ingredient, index) in ingredients"
+      <input-field
+        v-for="(mealDetail, index) in mealDetails"
+        :mealDetail="mealDetail"
         :key="index"
-      >
-        <input class="form-control" type="text" v-model="ingredients[index]" />
-      </div>
-      <div
-        class="form-group col-4"
-        v-for="(amount, index) in amounts"
-        :key="index"
-      >
-        <input class="form-control" type="text" v-model="amounts[index]" />
-      </div>
+      ></input-field>
     </div>
-    <div
-      class="batsu"
-      @click.prevent="deleteForm(index)"
-    >
-      ×
-    </div>
+    <div class="batsu" @click.prevent="deleteForm(index)">×</div>
 
     <button
       class="btn btn-secondary"
@@ -36,6 +19,7 @@
   </div>
 </template>
 <script>
+var data_array = ["1", "2", "3"];
 import { onMounted } from "vue";
 export default {
   setup() {
@@ -47,6 +31,7 @@ export default {
   },
   data() {
     return {
+      mealDetails: data_array,
       foods: [""],
       ingredients: [""],
       amounts: [""],
@@ -70,14 +55,25 @@ export default {
     },
 
     addForm() {
-      this.foods.push("");
-      this.ingredients.push("");
-      this.amounts.push("");
+      this.mealDetails.push("");
     },
     deleteForm(index) {
-      this.foods.splice(index, 1);
-      this.ingredients.splice(index, 1);
-      this.amounts.splice(index, 1);
+      this.mealDetails.splice(index, 1);
+    },
+  },
+  components: {
+    "input-field": {
+      template: `
+			<div class="form-group col-4">
+        <input class="form-control" type="text" v-model="mealDetail.foods" />
+      </div>
+      <div class="form-group col-4">
+        <input class="form-control" type="text" v-model="mealDetail.ingredients" />
+      </div>
+      <div class="form-group col-4">
+        <input class="form-control" type="text" v-model="mealDetail.amounts" />
+      </div>`,
+      props: ["mealDetail"],
     },
   },
 };
